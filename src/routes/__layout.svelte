095acee09885d7343dfff418b9fe5f2f43ad559d
Daniel Molina <danmnvx@gmail.com>
2022-06-08 22:06:39 -0500

    initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>Made by <a target="" href="https://danielmolina.me">DM</a></p>
+</footer>
+
+<style>
+	main {
+		
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Jhazzel & Edgar</title>
+	<meta name="description" content="Jhazzel & Edgar - Boda de invitación" />
+</svelte:head>
+
+<section>
+</section>
+
+<style>
+</style>
