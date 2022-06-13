9c8d47e1bbf2fb16bdb6179756aa979cd5036515
Daniel Molina <danmnvx@gmail.com>
2022-06-13 14:08:06 -0500

    Progress on counter

---
+++ src/lib/Countdown.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { onMount } from 'svelte';
+
+    let time = {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+    }
+
+    onMount(async () => {
+		const countdown = () => {
+            const countDate = new Date("September 24, 2022 17:00:00");
+            const now = new Date().getTime();
+            const gap = countDate - now;
+
+            const second = 1000;
+            const minute = second * 60;
+            const hour = minute * 60;
+            const day = hour * 24;
+
+            time.days = Math.floor(gap / day);
+            time.hours = Math.floor((gap % day) / hour);
+            time.minutes = Math.floor((gap % hour) / minute);
+            time.seconds = Math.floor((gap % minute) / second);
+        };
+
+        setInterval(() => countdown(), 1000);
+	});
+</script>
+
+<section>
+    <p>{time.days} {time.hours} {time.minutes} {time.seconds}</p>
+</section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,12 +2,29 @@
 	export const prerender = true;
 </script>
 
+<script>
+	import Countdown from '$lib/Countdown.svelte';
+</script>
+
 <svelte:head>
 	<title>Jhazzel & Edgar</title>
 	<meta name="description" content="Jhazzel & Edgar - Boda de invitación" />
 </svelte:head>
 
 <section>
+	<!-- Names -->
+	<section class="container py-10 flex flex-col gap-y-7">
+		<div class="bg-red-300 text-center uppercase">
+			<p>¡Nos casamos!</p>
+		</div>
+		<div class="bg-blue-200 text-center">
+			<h1 class="handwriting text-7xl">Jhazzel</h1>
+			<h3 class="handwriting text-5xl">&</h3>
+			<h1 class="handwriting text-7xl">Edgar</h1>
+		</div>
+	</section>
+
+	<Countdown />
 </section>
 
 <style>
