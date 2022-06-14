ea70c7c89b9aed3397d12980699c0a28cc57dbf8
Daniel Molina <danmnvx@gmail.com>
2022-06-13 20:52:07 -0500

    Headline to know more of the event

---
+++ src/lib/Headline.svelte
@@ -0,0 +1,63 @@
+<section>
+    <!-- Names -->
+	<section class="container py-10 flex flex-col gap-y-7">
+		<div class="text-center uppercase">
+			<p>¡Nos casamos!</p>
+		</div>
+		<div class="text-center">
+			<h1 class="handwriting text-7xl">Jhazzel</h1>
+			<h3 class="handwriting text-5xl">&</h3>
+			<h1 class="handwriting text-7xl">Edgar</h1>
+		</div>
+	</section>
+
+    <!-- Date -->
+    <section class="container pt-9">
+        <div class="grid grid-cols-3">
+            <!-- Day -->
+            <div class="h-48 pr-0">
+                <div class="table w-full h-full">
+                    <div class="table-cell align-middle">
+                        <div class="before:table-cell after:table-cell">
+                            <div class="py-2 border-y-2 border-primary">
+                                <p class="text-center tracking-widest text-lg uppercase">sábado</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Number -->
+            <div class="h-48 border-x-2 border-primary">
+                <div class="h-48 pr-0">
+                    <div class="w-full h-full">
+                        <div class="h-full">
+                            <div class="before:table-cell after:table-cell h-full">
+                                <div class="h-full flex flex-col justify-around">
+                                    <p class="text-center uppercase tracking-wider text-lg">4:00 pm</p>
+                                    <p class="text-center text-6xl handwriting">24</p>
+                                    <p class="text-center tracking-wider text-lg">2022</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Month -->
+            <div class="h-48 pl-0">
+                <div class="table w-full h-full">
+                    <div class="table-cell align-middle">
+                        <div class="before:table-cell after:table-cell">
+                            <div class="py-2 border-y-2 border-primary">
+                                <p class="text-center tracking-widest text-lg uppercase">septiembre</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</section>
+
+<style>
+
+</style>
