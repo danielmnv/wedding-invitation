f5f3b41cab41eaf685430f50ea247dafd343b0af
Daniel Molina <danmnvx@gmail.com>
2022-06-17 18:08:58 -0500

    Title component to show the title animated of the section

---
+++ src/lib/components/Title.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Saos from 'saos';
+
+    export let text = '', duration = 0.6;
+</script>
+
+<Saos
+    animation="fade-in-top {duration}s cubic-bezier(0.390, 0.575, 0.565, 1.000) both"
+    once={true}
+>
+    {#if $$slots.custom}
+    <slot name="custom"></slot>
+    {:else}
+    <p class="headline">{text}</p>
+    {/if}
+</Saos>
