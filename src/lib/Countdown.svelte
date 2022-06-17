8512ce01d01b39fc883db7748d6c85882298a8d5
Daniel Molina <danmnvx@gmail.com>
2022-06-16 21:02:32 -0500

    Countdown fixed

--- src/lib/Countdown.svelte
+++ src/lib/Countdown.svelte
@@ -3,7 +3,7 @@
     import CountdownSection from '$lib/CountdownSection.svelte';
     import { onMount } from 'svelte';
 
-    let time = {
+    let isStarting = false, time = {
         days: 0,
         hours: 0,
         minutes: 0,
@@ -12,9 +12,11 @@
 
     onMount(async () => {
 		const countdown = () => {
-            const countDate = new Date("September 24, 2022 17:00:00");
+            const countDate = new Date("September 24, 2022 14:00:00");
             const now = new Date().getTime();
             const gap = countDate - now;
+
+            if (!isStarting && gap == 0) isStarting = true;
 
             const second = 1000;
             const minute = second * 60;
@@ -27,32 +29,48 @@
             time.seconds = `${(Math.floor((gap % minute) / second))}`.padStart(2, '0');
         };
 
+        countdown();
         setInterval(() => countdown(), 1000);
 	});
+
+    function getUnit(index) {
+        switch (index) {
+            case 0:
+                return 'días';
+            case 1:
+                return 'hrs';
+            case 2:
+                return 'min';
+            case 3:
+                return 'seg';
+        }
+    }
 </script>
 
 <section>
     <p class="text-center uppercase p-9 tracking-widest">Sólo faltan:</p>
 
     <!-- Countdown -->
-    <div class="container bg-primary bg-opacity-85 px-8 md:px-28 md:rounded-md">
+    <div class="container bg-primary bg-opacity-85 px-8 md:px-28 sm:rounded-md">
         <Saos 
             animation={"fade-in 3s cubic-bezier(0.390, 0.575, 0.565, 1.000) both"}
             once={true}
         >
-            <div class="flex flex-row">
+            <div class="flex flex-row py-8 text-slate-50">
+                {#if !isStarting}
+                {#each Object.values(time) as number, i}
                 <div class="flex-1">
-                    <CountdownSection number={time.days} unit="días" />
+                    <CountdownSection number={number} unit={getUnit(i)} />
                 </div>
-                <div class="flex-1">
-                    <CountdownSection number={time.hours} unit="hrs" />
+                {/each}
+
+                {:else}
+                <div class="w-full text-center">
+                    <!-- TODO: add text and fireworks/animation -->
+                    <p class="pb-1 handwriting font-extrabold text-3xl md:text-5xl md:font-semibold">Start</p>
+                    <p class="uppercase tracking-widest font-extrabold text-xs md:text-sm md:font-semibold">Start</p>
                 </div>
-                <div class="flex-1">
-                    <CountdownSection number={time.minutes} unit="min" />
-                </div>
-                <div class="flex-1">
-                    <CountdownSection number={time.seconds} unit="seg" />
-                </div>
+                {/if}
             </div>
         </Saos>
     </div>
--- src/lib/CountdownSection.svelte
+++ src/lib/CountdownSection.svelte
@@ -2,7 +2,7 @@
     export let number, unit;
 </script>
 
-<div class="flex flex-col items-center gap-y-1 py-8 text-slate-50">
+<div class="flex flex-col items-center gap-y-1">
     <span class="handwriting font-extrabold text-3xl md:text-5xl md:font-semibold">{number}</span>
     <span class="uppercase tracking-widest font-extrabold text-xs md:text-sm md:font-semibold">{unit}</span>
 </div>
