70be49f2c3aaa294fce07af340e920b881da774f
Daniel Molina <danmnvx@gmail.com>
2022-06-16 19:44:07 -0500

    Section for gifts

---
+++ src/lib/CompanyGifts.svelte
@@ -0,0 +1,24 @@
+<script>
+    import IconGift from '$lib/icons/Gift.svelte';
+
+    export let company, image, border = false, isPNG = false;
+</script>
+
+<div class="px-3 py-8 md:p-6 {border ? 'border-b border-b-primary md:border-b-0 md:border-r md:border-r-primary' : ''}">
+    <div class="px-20 md:px-28">
+        <img src={image} alt={company} class:png-image="{isPNG}">
+    </div>
+
+    <button type="button" class="button mx-auto mt-8 md:mt-16">
+        <span>Ver regalos</span>
+        <IconGift />
+    </button>
+</div>
+
+<style>
+    .png-image {
+        /* TODO: if this color needs to be changeed, look for https://codepen.io/sosuke/pen/Pjoqqp */
+        /* filter: invert(82%) sepia(6%) saturate(3219%) hue-rotate(4deg) brightness(83%) contrast(100%); */
+        filter: invert(1);
+    }
+</style>
---
+++ src/lib/GiftArea.svelte
@@ -0,0 +1,19 @@
+<script>
+    import CompanyGifts from '$lib/CompanyGifts.svelte';
+</script>
+
+<section class="pt-20">
+
+    <div class="container">
+        <p class="text-center text-5xl handwriting">Regalos</p>
+    
+        <p class="text-center pt-16 uppercase tracking-wide">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit quibusdam debitis vel laboriosam, asperiores architecto, a reiciendis non tenetur quis eum impedit eligendi nostrum. Saepe harum aspernatur impedit fuga ducimus.</p>
+    </div>
+
+    <div class="container pt-28">
+        <div class="grid grid-cols-1 md:grid-cols-2">
+            <CompanyGifts company="Amazon" image="/amazon.svg" border={true} />
+            <CompanyGifts company="Cantia" image="/cantia.png" isPNG={true} />
+        </div>
+    </div>
+</section>
