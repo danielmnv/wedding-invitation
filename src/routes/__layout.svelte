da0969ddcfe0ece3b71bf9ea2ddce35815184a75
Daniel Molina <danmnvx@gmail.com>
2022-07-06 01:38:55 -0500

    Added author

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import '../app.css';
 	import { handleViewport } from '../viewport';
+
+	import Fa from 'svelte-fa';
+	import { faArrowUpRightFromSquare, faHeart } from '@fortawesome/free-solid-svg-icons'
 </script>
 
 <div class="main-content" use:handleViewport>
@@ -9,6 +12,25 @@
 	</main>
 	
 	<footer>
-		<p>Made by <a target="" href="https://danielmolina.me">DM</a></p>
+		<div class="bg-slate-600 text-white p-2 flex items-center justify-between gap-x-5">
+			<div class="w-8">
+				<a href="https://danielmolina.me" target="_blank">
+					<img src="dm.svg" alt="Daniel Molina">
+				</a>
+			</div>
+
+			<div class="flex items-center gap-x-2 font-bold text-sm md:text-base">
+				<span>Made with </span>
+				<Fa icon={faHeart} /> 
+				<span>by</span>
+				<span>Daniel Molina!</span>
+			</div>
+
+			<div class="flex justify-end">
+				<a href="https://danielmolina.me" target="_blank">
+					<Fa icon={faArrowUpRightFromSquare} />
+				</a>
+			</div>
+		</div>
 	</footer>
 </div>
