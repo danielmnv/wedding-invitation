b9e0a62d795e7bfb5b0879231d414e5a29d12d5f
Daniel Molina <danmnvx@gmail.com>
2022-08-14 22:12:36 -0500

    Loader style changed

--- src/lib/Loader.svelte
+++ src/lib/Loader.svelte
@@ -8,13 +8,13 @@
             animation="heartbeat 1.5s ease-in-out infinite both"
         >
             <div class="flex justify-center">
-                <div class="circle-container">
+                <div class="circle-container loader-text">
                     <span class="first-letter">J</span>
                     <span class="second-letter">A</span>
                 </div>
             </div>
 
-            <img class="absolute center-item w-auto h-[17rem] -ml-4 mt-2" src="ring.png" alt="Ring">
+            <img class="absolute center-item w-auto h-[17rem] -ml-2" src="ring.webp" alt="Ring">
         </Saos>
     </div>
 </section>
@@ -24,18 +24,18 @@
     @apply 
         relative
         bg-white rounded-full p-28 shadow-lg
-        text-primary text-9xl font-extrabold;
+        text-primary;
 }
 
 .first-letter {
     @apply
-        absolute
+        absolute text-8xl
         top-1/3 left-1/3 -translate-y-1/3 -translate-x-1/3;
 }
 
 .second-letter {
     @apply
-        absolute
+        absolute text-9xl
         bottom-1/3 right-1/3 translate-y-1/3 translate-x-1/3;
 }
 </style>
